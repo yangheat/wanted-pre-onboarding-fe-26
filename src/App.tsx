02e7da6541c6b539fff9a9e05c7c310132a83898
yangheat <yangheat@gamil.com>
2024-09-29 23:38:33 +0900

    갱신 기능 추가 - 로딩 부분 수정 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,93 @@
-import { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { getMockData } from './api/products'
 import './App.css'
 import { MockData } from './types/mock'
 
-function TotalPrice({ totalPrice }: { totalPrice: number}) {
-  return <p>TotalPrice: { totalPrice }</p>
+function TotalPrice({ totalPrice }: { totalPrice: number }) {
+  return <p>TotalPrice: {totalPrice}</p>
 }
 
-function ProductList({ products }: { products: MockData[]}) {
+function ProductList({
+  products,
+  setPageNum,
+  isEnd
+}: {
+  products: MockData[]
+  setPageNum: React.Dispatch<React.SetStateAction<number>>
+  isEnd: boolean
+}) {
+  const [load, setLoad] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && isEnd === false) {
+          setLoad(true)
+          setPageNum((curr) => curr + 1)
+        }
+      },
+      { threshold: 0.9 }
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current)
+      }
+    }
+  }, [])
   return (
-    <>
+    <section className="product-list-container">
       <table>
         <tbody>
-            {products.map((product, index) => (
-              <tr key={index}>
-                {Object.entries(product).map(([key, value]) => (
-                  <td key={index + key + value}>{key}: {value}</td>
-                ))}
-              </tr>
-            ))}
+          {products.map((product, index) => (
+            <tr key={index}>
+              {Object.entries(product).map(([key, value]) => (
+                <td key={index + key + value}>{value}</td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
-      
-    </>
+      <div ref={ref}>{load ? 'loading...' : ''}</div>
+    </section>
   )
 }
 
 function App() {
   const [products, setProducts] = useState<MockData[]>([])
+  const [isEnd, setIsEnd] = useState(false)
   const [totalPrice, setTotalPrice] = useState(0)
-  const [pageNum, setpageNum] = useState(0)
+  const [pageNum, setPageNum] = useState(0)
+
+  let temp: MockData[] = []
 
   useEffect(() => {
     getMockData(pageNum).then((response) => {
+      temp = response.datas
+      setProducts([...products, ...response.datas])
+
       let price = 0
-      response.datas.forEach((data) => {
+      response.datas.forEach((data, index) => {
         price += data.price
       })
-      setTotalPrice(price)
-      setProducts(response.datas)
+      setTotalPrice((curr) => curr + price)
+
+      setIsEnd(response.isEnd)
     })
-  }, [])
+  }, [pageNum])
+
   return (
     <>
-      <TotalPrice totalPrice={totalPrice}/>
-      <ProductList products={products}/>
+      <header>
+        <TotalPrice totalPrice={totalPrice} />
+      </header>
+
+      <ProductList products={products} setPageNum={setPageNum} isEnd={isEnd} />
     </>
   )
 }
