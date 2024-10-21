34220c489d64454e6bf33e8447a14c2873765a98
yangheat <yangheat@gamil.com>
2024-10-21 22:32:54 +0900

    feat: 의존성 배열 제거하여 리팩터링

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,66 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getMockData } from './api/products'
-import './App.css'
 import { MockData } from './types/mock'
-
-const OBSERVER_THRESHOD = 0.9
-
-type TProductList = {
-  products: MockData[]
-  setPageNum: React.Dispatch<React.SetStateAction<number>>
-  isEnd: boolean
-}
-
-function TotalPrice({ totalPrice }: { totalPrice: number }) {
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(totalPrice)
-  return (
-    <header>
-      <p>TotalPrice: {formattedPrice}</p>
-    </header>
-  )
-}
-
-function ProductItem({ products }: { products: MockData[] }) {
-  return (
-    <table>
-      <tbody>
-        {products.map((product, index) => (
-          <tr key={index}>
-            {Object.entries(product).map(([key, value]) => (
-              <td key={`${key}-${value}`}>{value}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
-}
-
-function ProductList({ products, setPageNum, isEnd }: TProductList) {
-  const [isLoading, setIsLoading] = useState(false)
-  const observerRef = useRef(null)
-
-  const handleInterSection = useCallback(
-    (entries: IntersectionObserverEntry[]) => {
-      if (entries[0].isIntersecting && products.length > 0 && !isEnd) {
-        setIsLoading(true)
-        setPageNum((curr) => curr + 1)
-      }
-    },
-    [products.length, isEnd, setPageNum]
-  )
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleInterSection, {
-      threshold: OBSERVER_THRESHOD
-    })
-    const currntRef = observerRef.current
-
-    if (currntRef) {
-      observer.observe(currntRef)
-    }
-
-    return () => {
-      if (currntRef) {
-        observer.unobserve(currntRef)
-      }
-    }
-  }, [handleInterSection])
-
-  useEffect(() => {
-    if (isEnd) {
-      setIsLoading(false)
-    }
-  }, [isEnd])
-
-  return (
-    <>
-      <ProductItem products={products} />
-      <div ref={observerRef}>{isLoading && 'loading...'}</div>
-    </>
-  )
-}
 
 function App() {
   const [products, setProducts] = useState<MockData[]>([])
+  const [isLoading, setIsLoading] = useState(true)
   const [isEnd, setIsEnd] = useState(false)
   const [totalPrice, setTotalPrice] = useState(0)
-  const [pageNum, setPageNum] = useState(0)
+  const page = useRef(0)
+  const observerRef = useRef(null)
 
-  const fetchProducts = useCallback(async (page: number) => {
-    try {
-      const response = await getMockData(page)
-      const { datas, isEnd } = response
+  // 현재 isEnd, isLoading은 useEffect 내부에서만 값이 변경되기 때문에 의존성 배열에 설정할 필요가 없음
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      ([entrie]) => {
+        if (!entrie.isIntersecting || !isLoading || isEnd) {
+          return
+        }
 
-      setProducts((prev) => [...prev, ...datas])
+        setIsLoading(true)
 
-      const newPrice = datas.reduce((sum, data) => sum + data.price, 0)
-      setTotalPrice((prev) => prev + newPrice)
-      setIsEnd(isEnd)
-    } catch (error) {
-      console.error('product fetch error')
+        getMockData(page.current)
+          .then((result) => {
+            setProducts((prev) => [...prev, ...result.datas])
+            setIsEnd(result.isEnd)
+            page.current += 1
+            setTotalPrice((prev) =>
+              result.datas.reduce((acc, cur) => acc + cur.price, prev)
+            )
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setIsLoading(false))
+      },
+      { threshold: 0.9 }
+    )
+
+    const currentRef = observerRef.current
+    if (currentRef) {
+      intersectionObserver.observe(currentRef)
+    }
+
+    return () => {
+      if (currentRef) {
+        intersectionObserver.unobserve(currentRef)
+      }
     }
   }, [])
 
-  useEffect(() => {
-    if (isEnd) {
-      return
-    }
-    fetchProducts(pageNum)
-  }, [pageNum, fetchProducts])
-
   return (
     <>
-      <TotalPrice totalPrice={totalPrice} />
-      <ProductList products={products} setPageNum={setPageNum} isEnd={isEnd} />
+      <>Total Price: {totalPrice}</>
+      <section>
+        {products.map((product, index) => (
+          <div key={product.productId + index}>
+            {Object.entries(product).map(([key, value]) => (
+              <p key={`${key}-${value}`}>{value}</p>
+            ))}
+          </div>
+        ))}
+      </section>
+      <div ref={observerRef}>{isLoading ? 'loading...' : ''}</div>
     </>
   )
 }
